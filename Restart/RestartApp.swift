7950c9a424c560d251d3cdebe2789e9552033f3e
Viacheslav Dulnev <viacheslav.dulnev@medtronic.com>
2022-01-12 17:44:07 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Viacheslav Dulnev on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
